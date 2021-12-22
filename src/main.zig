7e90b6143119b5f6ed87440f809d7d42267e1044
Mateusz Czapliński <czapkofan@gmail.com>
2021-12-22 01:20:24 +0100

    build: use page_allocator instead of testing one

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
 const lua = @import("lua");
 const autolua = @import("autolua");
+var allocator = std.heap.page_allocator;
 
 pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
 
-    const L = try autolua.newState(std.testing.allocator);
+    const L = try autolua.newState(&allocator);
     defer lua.lua_close(L);
     lua.luaL_openlibs(L);
 }
